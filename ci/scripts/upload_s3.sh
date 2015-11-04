0f893b93f8e7f2f1e3251f8118547a05c21e6590
Kara Felix <karairenefelix@gmail.com>
2015-11-04 11:54:00 -0800

    adding extension

--- ci/scripts/upload_s3.sh
+++ ci/scripts/upload_s3.sh
@@ -2,12 +2,4 @@
 
 mkdir out
 
-echo "fish" > out/stemcell-version
-
-cd concourse-demo/ci
-
-mkdir out
-
-echo "fish2" > out/stemcell-version
-
-echo "Finished"
+echo "fish" > out/stemcell-version.blah
